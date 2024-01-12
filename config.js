8cd2031e483144f4d932faccc2ba93d958ca98cd
Ly Thanh Tung <lt_tung@brycen-vn.local>
2024-01-12 09:45:49 +0700

    init

---
+++ config.js
@@ -0,0 +1,27 @@
+var config = {
+    
+    texts: [
+        "Chúc Sô tuổi mới",
+        "Thật nhiều niềm vui",
+        "Luôn xinh đẹp",
+        "May mắn",
+        "Hạnh phúc",
+        "Thành công",
+        "Đu đu!!!",
+    ],
+    
+    imgs: {
+        "Chúc mừng sinh nhật": "./imgs/sony.png", 
+    },
+    
+    desc: {
+        turn_on: "Đen thui à, bật đèn lên đi!",
+        play: "Im im rứa, cho tôi con beat đê!",
+        bannar_coming: "Thêm tí màu mè nè!",
+        balloons_flying: "Cho mấy quả bong bóng bay chill chill đi!",
+        cake_fadein: "Cho xin cái bánh kem =))",
+        light_candle: "Thăp nến lên mà thổi, lẹ cái tay!",
+        wish_message: "Ước lẹ ước lẹ!",
+        story: "Ước rồi thì kích tiếp mà xem, chần chờ gì nữa!",
+    }
+};
